149f466d07f135b19bd01921edbf10af7904dc0a
Lucas S <lucas.santos.silva2011@gmail.com>
2022-11-28 22:47:05 -0300

    Permitir cadastro de sites

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("------- MEUS SITES FAVORITOS --------");
+
+        Scanner scanner = new Scanner(System.in);
+        List<Website> meusWebsites = new ArrayList<>();
+
+
+        String nome = scanner.nextLine();
+        String url = scanner.nextLine();
+
+        Website website = new Website();
+        website.setNome(nome);
+        website.setUrl(url);
+
+        meusWebsites.add(website);
     }
 }
